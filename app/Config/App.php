8bcea46a07d40072295ff0cbce37ba0471dfd387
Ewen BILLOT <alsc-adaialt20-bew@ccicampus.fr>
2022-01-17 16:12:37 +0100

    changement Url nom du site

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://halo/haloApiProjectPOO/public';
+    public $baseURL = 'http://halo';
 
     /**
      * --------------------------------------------------------------------------
