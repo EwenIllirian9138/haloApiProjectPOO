f28aca6c77b69a752d65e69c2ea82ca750125a8f
leoe67 <leo.ehlenberger@gmail.com>
2022-01-19 16:16:36 +0100

    Ajout bibliothèque jquery Datatables + modification liste superheros

--- app/Views/superheroes_list.php
+++ app/Views/superheroes_list.php
@@ -6,27 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title; ?></title>
     <link rel="shortcut icon" type="image/png" href="<?php echo base_url('favicon.ico');?>"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.11.3/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.11.3/datatables.min.js"></script>
 </head>
 <body>
 <h1><?php echo $title; ?></h1>
 
-<p><a href="<?php echo previous_url();?>">Retour</a></p>
-<table>
+<script>
+    $(document).ready(function() {
+        $('#SuperHeroesList').DataTable();
+    } );
+</script>
+
+
+<table id="SuperHeroesList" class="display" style="width:100%">
     <thead>
     <tr>
+        <th>Image</th>
         <th>Nom</th>
-        <th>Alignement</th>
-        <th>Alias</th>
-        <th>Lieu de naissance</th>
     </tr>
     </thead>
     <tbody>
     <?php foreach($arrSuperHeroes as $row){ ?>
         <tr>
+            <td><img src="<?php echo $row->ImageLink;?>" width="80"></td>
             <td><?php echo $row->Name;?></td>
-            <td><?php echo $row->Alignment;?></td>
-            <td><?php echo $row->Aliases = $row->Aliases == null ? " RAS////// " : $row->Aliases;?></td>
-            <td><?php echo $row->PlaceOfBirth = $row->PlaceOfBirth == null ? "RAS/////" : $row->PlaceOfBirth;?></td>
         </tr>
     <?php } ?>
     </tbody>
