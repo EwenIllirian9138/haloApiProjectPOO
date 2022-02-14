48cd4790603994d0669310f2ce936ade46f6bf9d
HKLegrand <hugo.legrand11laposte.net>
2022-02-14 12:00:23 +0100

    Ajout des champs IdApi et Published de la bdd

---
+++ app/Views/signup_view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+<h1><?php echo $title; ?></h1>
+<?php
+echo $form_open;
+echo $label_email;
+echo $form_email;
+echo $label_password;
+echo $form_password;
+echo $form_submit;
+echo $form_close;
+
+if (count($arrErrors) > 0){
+    ?>
+    <div>
+        <?php
+        foreach($arrErrors as $strError){
+            echo "<p>".$strError."</p>";
+        }
+        ?>
+    </div>
+    <?php
+}
+?>
+
+</body>
+</html>
