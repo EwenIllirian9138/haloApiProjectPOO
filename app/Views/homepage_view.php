95b5a686bde995213cc29986e1ab7464ed15878f
HKLegrand <hugo.legrand11laposte.net>
2022-02-14 09:32:59 +0100

    Ajout des champs IdApi et Published de la bdd

--- app/Views/homepage_view.php
+++ app/Views/homepage_view.php
@@ -10,15 +10,16 @@
 <h1>
     <?php echo $title; ?>
 </h1>
-<div style="section:flex;">
+<div><table><tr><td><a href="Homepage">Accueil</a></td><td><a href="SuperHeroesAPI">Liste des SuperHéros</a></td></tr></table></div>
+<div style="section:flex; flex-direction: row;">
     <?php for($i = 0 ; $i<=2 ; $i++){ ?>
     <div>
-        <div style="section:flex;"><img src="<?php echo $arrNewSuperHeroes[$i]->ImageLink; ?>"></div>
-        <div style="section:flex; flex-directon:column;">
-            <div style="section:flex;"><h3>Nom : <?php echo $arrNewSuperHeroes[$i]->Name; ?></h3></div>
-            <div style="section:flex;">Alter-Ego : <?php echo $arrNewSuperHeroes[$i]->AlterEgo; ?></div>
-            <div style="section:flex;">Alignement : <?php echo $arrNewSuperHeroes[$i]->Alignment; ?></div>
-            <div style="section:flex;">Première apparition : <?php echo $arrNewSuperHeroes[$i]->FirstAppearance; ?></div>
+        <div style="section:flex; flex-direction: row;"><img src="<?php echo $arrNewSuperHeroes[$i]->ImageLink; ?>"></div>
+        <div style="section:flex, flex-directon:column;">
+            <div style="section:flex; flex-direction: row;"><h3>Nom : <?php echo $arrNewSuperHeroes[$i]->Name; ?></h3></div>
+            <div style="section:flex; flex-direction: row;">Alter-Ego : <?php echo $arrNewSuperHeroes[$i]->AlterEgo; ?></div>
+            <div style="section:flex; flex-direction: row;">Alignement : <?php echo $arrNewSuperHeroes[$i]->Alignment; ?></div>
+            <div style="section:flex; flex-direction: row;">Première apparition : <?php echo $arrNewSuperHeroes[$i]->FirstAppearance; ?></div>
         </div>
     </div>
     <?php } ?>
